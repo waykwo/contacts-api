644e0e7f4aade09a1ad9df785c5266dad732a1ed
waykwo <72991933+waykwo@users.noreply.github.com>
2024-09-01 12:40:59 -0400

    Create routes for one contact and all contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,37 @@
 class ContactsController < ApplicationController
+  def first_contact
+    contact = Contact.find_by(id: 1)
+    p contact
+    render json: {
+      first_name: contact.first_name,
+      last_name: contact.last_name,
+      email: contact.email,
+      phone_number: contact.phone_number
+    }
+  end
+
+  # def all_contacts
+  #   all_contacts = Contact.all
+  #   p all_contacts
+  #   all_contacts_array = Array.new
+  #   all_contacts.each do |contact|
+  #     all_contacts_array << contact
+  #   end
+  #   render json: {contacts: all_contacts_array}
+  # end
+
+  def all_contacts
+    all_contacts = Contact.all
+    p all_contacts
+    all_contacts_array = Array.new
+    all_contacts.each do |contact|
+      all_contacts_array << {
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        email: contact.email,
+        phone_number: contact.phone_number
+      }
+    end
+    render json: {contacts: all_contacts_array}
+  end
 end
